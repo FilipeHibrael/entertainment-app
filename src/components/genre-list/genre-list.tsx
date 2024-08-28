af1a9106aec5047544f12db3d52abbfb51aa7f1c
FilipeHibrael <filipehibrael27@gmail.com>
2024-08-28 19:31:47 -0300

    genre list

--- src/components/genre-list/genre-list.tsx
+++ src/components/genre-list/genre-list.tsx
@@ -1,6 +1,7 @@
 import getGenreList from '@/actions/get-genre-list';
 import { MediaType } from '@/types/types';
 import Link from 'next/link';
+import styles from './genre-list.module.css';
 
 type GenreListProps = {
   mediaType: MediaType;
@@ -9,12 +10,18 @@ type GenreListProps = {
 export default async function GenreList({ mediaType }: GenreListProps) {
   const { data, error } = await getGenreList(mediaType);
 
+  function formatGenreName(name: String) {
+    return name.replace(/&/g, 'and').replace(/\s+/g, '-').toLowerCase();
+  }
+
   if (!data?.genres) return <p>{error}</p>;
   return (
-    <ul>
+    <ul className={`${styles.genreList} ${styles[mediaType]}`}>
       {data.genres.map((genre) => (
-        <li key={genre.id}>
-          <Link href={''}>{genre.name}</Link>
+        <li key={genre.id} className={styles.genreLink}>
+          <Link href={`/${mediaType}/genre/${formatGenreName(genre.name)}`}>
+            {genre.name}
+          </Link>
         </li>
       ))}
     </ul>
