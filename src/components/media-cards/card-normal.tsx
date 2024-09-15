c91176f7e8c7a9bdc8c6cda0bca28f7a85455270
FilipeHibrael <filipehibrael27@gmail.com>
2024-09-15 11:10:37 -0300

    default image

--- src/components/media-cards/card-normal.tsx
+++ src/components/media-cards/card-normal.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { API_IMAGE_PATH } from '@/functions/api';
 import { Media, MediaType, isMovieMedia, isTvMedia } from '@/types/types';
 import Image from 'next/image';
@@ -5,6 +6,7 @@ import styles from './card-normal.module.css';
 import Link from 'next/link';
 import MovieCardText from './cards-text/movie-card-text';
 import TvCardText from './cards-text/tv-card-text';
+import React from 'react';
 
 type CardNormalProps = {
   media: Media;
@@ -12,13 +14,21 @@ type CardNormalProps = {
 };
 
 export default function CardNormal({ media, mediaType }: CardNormalProps) {
-  const urlImage = API_IMAGE_PATH + media.backdrop_path;
+  const [urlImage, setUrlImage] = React.useState(
+    API_IMAGE_PATH + media.backdrop_path
+  );
 
   return (
     <li className={styles.card}>
       <Link href={`/${mediaType}/id/${media.id}`}>
         <div className={styles.image}>
-          <Image alt="media image" src={urlImage} width={480} height={720} />
+          <Image
+            alt="media image"
+            src={urlImage}
+            width={480}
+            height={720}
+            onError={() => setUrlImage('/images/default-image.svg')}
+          />
         </div>
         {isMovieMedia(media) && (
           <MovieCardText title={media.title} releaseDate={media.release_date} />
